929281beb6d96f0c2150d7a143665b4fffc82b84
2019e166@eng.jfn.ac.lk <2019e166@eng.jfn.ac.lk>
2022-10-11 21:15:33 +0530

    Added user interfaces.
    New user interface for sellers was created. Some edits were done for the other pages also.

---
+++ registerHome.php
@@ -0,0 +1,15 @@
+<?php
+require_once 'inc/header.php';
+?>
+<div>
+<h3>Register Page</h3>
+<p> Already have an account <a href="login.php">Login!</a></p>
+<p> Return to Home Page <a href="index.php">Home Page</a></p>
+<form >
+	<button type="submit" formaction="register.html">Register as customer</button>
+	<button type="submit" formaction="registerSellers.php">Register as seller.</button>
+</form>
+</div>
+<?php
+require_once 'inc/footer.php';
+?>
---
+++ registerSellers.php
@@ -0,0 +1,18 @@
+<?php
+require_once 'inc/header.php';
+?>
+<div>
+<h3>Register for Sellers.</h3>
+<p> No Account <a href="register.html">Register Now for customers!</a></p>
+<p> Return to Home Page <a href="index.php">Home Page</a></p>
+<p> Already have an account <a href="login.php">Login!</a></p>
+<form action="" method="post">
+	<input type="text" name="Username" placeholder="UserName">
+	<input type="password" name="password" placeholder="Password">
+	<button type="submit">Login</button>
+</form>
+<p> Back to registration home page. <a href="registerHome.php"> Back </a></p>
+</div>
+<?php
+require_once 'inc/footer.php';
+?>
