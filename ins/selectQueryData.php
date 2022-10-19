82a905ca13a681d3f56f17201e58d7663faefb7e
2019e166@eng.jfn.ac.lk <2019e166@eng.jfn.ac.lk>
2022-10-19 23:59:43 +0530

    Update selectQueryData.php

--- ins/selectQueryData.php
+++ ins/selectQueryData.php
@@ -1,1 +1,12 @@
 <?php require_once('inc/connection.php'); ?>
+<?php 
+	$query = "SELECT CustomerId,CustomerName,Mobile,Address,Email FROM CUSTOMERDETAILS";
+	$result = mysqli_query($connection,$query);
+	if($result)
+	{
+		echo mysqli_num_rows($result)."Found";
+
+		$table = '<table>';
+		$table = '<tr><th>ID</th><th>' 
+
+	}
